2e31ece83d1bd89c5a7572da9247e503b02b97f2
troob <troobengineer@gmail.com>
2024-07-28 19:21:44 -0400

    init

---
+++ compare_int_float.ino
@@ -0,0 +1,18 @@
+void setup() {
+  Serial.begin(9600);
+
+  int i;
+  float f;
+  i = 3/4;
+  Serial.println(i);
+  f=3/4;
+  Serial.println(f);
+  f=3/4*2.;
+  Serial.println(f);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
